89cb4df3c4fce404df328e161b2a20e4b3f4135a
Hizkia sekeon <145413729+hizkiarenvil@users.noreply.github.com>
2024-03-19 13:27:33 +0800

    Update constants.py

--- YoutubeAnalytics/constants.py
+++ YoutubeAnalytics/constants.py
@@ -1,9 +1,17 @@
 import configparser
 import os
 
+# Create a ConfigParser instance
 parser = configparser.ConfigParser()
 
-parser.read(os.path.join(os.path.dirname(__file__), 'config/config.local'))
+# Read the local config file
+# `os.path.join` constructs the path to the config file by joining the directory path of the current file with the relative path to the config file
+# __file__ contains the path of the current file
+# 'config/config.local' is the relative path to the config file
+config_file_path = os.path.join(os.path.dirname(__file__), 'config/config.local')
+parser.read(config_file_path)
 
+# Get the YouTube API key and playlist ID from the config file
+# These values are expected to be under the section 'youtube' in the config file
 YOUTUBE_API_KEY = parser.get('youtube', 'API_KEY')
 PLAYLIST_ID = parser.get('youtube', 'PLAYLIST_ID')
